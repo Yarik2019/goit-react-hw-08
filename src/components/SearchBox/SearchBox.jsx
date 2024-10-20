2156f07c2d642863702861221b4f463fdccd368a
Yarik2019 <yroslavterno@gmail.com>
2024-10-20 22:14:29 +0300

    add style

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -15,7 +15,8 @@ const SearchBox = () => {
         Find contacts by name
       </label>
       <input
-        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring shadow-md shadow-white transition-all ease-in duration-300
+      hover:scale-105"
         id={searchId}
         type="text"
         value={filter}
--- src/components/UpdateContactForm/UpdateContactForm.jsx
+++ src/components/UpdateContactForm/UpdateContactForm.jsx
@@ -53,7 +53,8 @@ const UpdateContactForm = ({ contact, closeModal }) => {
                   onChange={handleChange}
                   name="name"
                   type="text"
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring shadow-md shadow-white transition-all ease-in duration-300
+      hover:scale-105"
                 />
                 <ErrorMessage
                   name="name"
@@ -75,7 +76,8 @@ const UpdateContactForm = ({ contact, closeModal }) => {
                   onChange={handleChange}
                   name="number"
                   type="phone"
-                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring shadow-md shadow-white transition-all ease-in duration-300
+      hover:scale-105"
                 />
                 <ErrorMessage
                   name="phone"
